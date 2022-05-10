d71f1c87dc7f15e7f690f8a254b0195bd5aa69c5
wehai <83722180+wehai@users.noreply.github.com>
2022-05-10 20:10:58 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+let w = 0;
+function preload() {
+  fontCirrusCumulus = loadFont('assets/CirrusCumulus.otf');
+}
+function setup(){
+createCanvas(windowWidth, windowHeight);
+background(0,90,128);
+fill(250);
+rect(0,0,width,height*5/6);
+frameRate(500);
+}
+
+
+function draw(){
+  
+  
+fill(w);
+textSize(width/13);
+  textFont(CirrusCumulus)
+text("A B S C H I C K E N", width*1/6,height*11.5/12);
+
+  
+  noStroke();
+  fill(0);
+  if(mouseIsPressed){
+    if(mouseY<height*5/6){
+circle(mouseX,mouseY,7);
+    }
+  }
+}
+
+function mouseIsPressed() {
+ 
+  if(mouseY > 500){ 
+  w= 100;
+  saveCanvas("Zyklop"+day()+","+hour()+":"+minute()+"Uhr"+".jpg");
+}
+}
+
+function mouseReleased() {
+  
+  w= 230;
+}
